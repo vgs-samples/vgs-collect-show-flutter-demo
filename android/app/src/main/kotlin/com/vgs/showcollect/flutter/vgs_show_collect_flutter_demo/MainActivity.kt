4f28af2ecfae3df0bc195adc07ae1e0e1492e8e4
Dmytro <dmitriy.kosolapenko@vgs.io>
2022-09-22 16:53:24 +0300

    init collect

--- android/app/src/main/kotlin/com/vgs/showcollect/flutter/vgs_show_collect_flutter_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/vgs/showcollect/flutter/vgs_show_collect_flutter_demo/MainActivity.kt
@@ -4,18 +4,28 @@ import com.vgs.showcollect.flutter.vgs_show_collect_flutter_demo.view.collect.Ca
 import com.vgs.showcollect.flutter.vgs_show_collect_flutter_demo.view.reveal.CardShowViewFactory
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import com.verygoodsecurity.vgscollect.core.VGSCollect
 
 class MainActivity : FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         val registry = flutterEngine.platformViewsController.registry
-        registry.registerViewFactory(COLLECT_FORM_VIEW_TYPE, CardCollectViewFactory(flutterEngine.dartExecutor.binaryMessenger))
-        registry.registerViewFactory(SHOW_FORM_VIEW_TYPE, CardShowViewFactory(flutterEngine.dartExecutor.binaryMessenger))
+        registry.registerViewFactory(
+            COLLECT_FORM_VIEW_TYPE,
+            CardCollectViewFactory(
+                flutterEngine.dartExecutor.binaryMessenger,
+                VGSCollect(this, VAULT_ID, ENVIRONMENT)
+            )
+        )
+        registry.registerViewFactory(
+            SHOW_FORM_VIEW_TYPE,
+            CardShowViewFactory(flutterEngine.dartExecutor.binaryMessenger)
+        )
     }
 
     companion object {
 
-        const val VAULT_ID = "<YOUR_VAULT_ID>"
+        const val VAULT_ID = "tntpszqgikn"
         const val ENVIRONMENT = "sandbox"
 
         const val COLLECT_FORM_VIEW_TYPE = "card-collect-form-view";
